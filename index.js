6c195598fac1878cda7a31399fc574fba68e14da
costin88boss <42942503+costin88boss@users.noreply.github.com>
2021-01-31 19:04:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var prefix = ';'
 var Usersapplying = [{id:Number, timestamp:Number}]
 var ApplyChannel = null
 var CommandsChannel = null
+var ApplyRole = null
+var Builder, Adventurer, Pvper;
 
 bot.once('ready', () => {
 
@@ -25,7 +27,10 @@ bot.on('message', message => {
         .setTitle("General")
         embedModeration = new Discord.MessageEmbed()
         .addFields(
-            {name:'AC', value:'set the channel to send applications.'}
+            {name:'applyroles', value:'set the channel to send applications.'},
+            {name:'applyneededrole', value:"set the needed role to access moderation commands. THIS COMMAND IS ONLY ACCESSIBLE BY COSTIN"},
+            {name:'applychannel', value:'set the channel to log applications.'},
+            {name:'accept [USER] [IsPvp][IsBuild][IsAdvent]', value:'accept the user. replace the [Is...] with 0 or 1 if they will get those roles.'}
         )
         .setColor('#535C69')
         .setFooter("Made by Costin88boss")
@@ -116,12 +121,98 @@ bot.on('message', message => {
                     })
                 })
             })
-    }}
-    if (message.content.startsWith(prefix + "AC")){
+    } else message.reply("An error has occured, the apply channel has not been setup!") }
+    if (message.content.toLowerCase().startsWith(prefix + "applychannel")){
+        if (message.author.id == "471746995737067554"){
         ApplyChannel = message.mentions.channels.first();
+        message.channel.send("done");}
+        else message.channel.send("You are not costin. failure.");
     }
+    if (message.content.toLowerCase().startsWith(prefix + "applyneededrole")){
+        if (message.author.id == "471746995737067554")
+        if (message.mentions.roles.size == 1){
+        ApplyRole = message.mentions.roles.first();
+        message.channel.send("done");
+    }
+        else message.channel.send("There is no role!");
+    }
+    if (message.content.toLowerCase().startsWith(prefix + "applyroles")){
+        if (ApplyRole != null){
+        if (message.member.roles.cache.has(ApplyRole) || message.author.id == "471746995737067554")
+        if (message.mentions.roles.size == 0){
+            message.channel.send("There must be 3 roles!");
+            message.channel.send("also, the order is this: Pvper, Builder, Adventurer")
+        }
+        else if (message.mentions.roles.size == 3){
+            let Args = message.content.split(' ');
+            Pvper = getRoleFromMention(Args[1], message)
+            Builder = getRoleFromMention(Args[2], message)
+            Adventurer = getRoleFromMention(Args[3], message)
+            message.channel.send("done");
+        }
+        else if (message.mentions.roles.size >= 4){
+            message.channel.send("Why are you setupping more than 3 roles? don't you need only pvper, builder, and adventurer?");
+        }}
+    
+    else message.channel.send("Costin is an idiot for not setupping the perm role.");
+    }
+    if(message.content.toLowerCase().startsWith(prefix + "accept"))
+    {
+        let Args = message.content.split(' ');
+        if (Args[1] != null){
+        if (Args[1].match(Discord.MessageMentions.USERS_PATTERN) != null){
+            let User = Args[1];
+            User = User.replace(/(<@!)/, '');
+            User = User.replace(/(>)/, '');
+            if (Usersapplying.filter(e => e.id == User).length >= 1){
+            if (Args[2] == 0 || Args[2] == 1){
+                if(Args[3] == 0 || Args[3] == 1){
+                    if (Args[4] == 0 || Args[4] == 1){
+                        console.log(Builder)
+                        let Member = message.guild.members.cache.find(e => e.id == User);
+                            let pvper
+                            let builder
+                            let adventurer
+                            let GotRoles
+                            let Once
+                            GotRoles = " ";
+                            if (Args[2] == 1){
+                                pvper = true
+                                GotRoles += "Pvper "
+                            }
+                            if (Args[3] == 1){
+                                builder = true;
+                                GotRoles += "Builder "
+                                }
+                            if (Args[4] == 1){
+                                adventurer = true;
+                                GotRoles += "Adventurer "
+                            }
+                            message.channel.send("User is now a member. if he doesn't have the roles, then this bot doesn't have the permission to.")
+                            bot.users.cache.find(e => e.id == User).send("Your application has been accepted! roles given by the bot:" + GotRoles + ". if you got no roles, then it means this bot doesn't have  the permission to.")
 
-})
+                            if (pvper)
+                            Member.roles.add(Pvper).catch(() => {});
+                            if (builder)
+                            Member.roles.add(Builder).catch(() => {});
+                            if (adventurer)
+                            Member.roles.add(Adventurer).catch(() => {});
+                    }
+                    else{
+                        message.channel.send("the second argument should be 0 or 1 whetever the player is applied as Adventurer.");
+                    }
+                }
+                else{
+                    message.channel.send("the second argument should be 0 or 1 whetever the player is applied as Builder.");
+                }
+            }
+            else message.channel.send("the second argument should be 0 or 1 whetever the player is applied as Pvper.");} else
+            message.channel.send("the user didn't applied!")
+        } else message.channel.send("first argument isn't a user!")
+    }else message.channel.send("you must enter the arguments!");
+}
+}
+)
 
 function SendToChannel(Answers, User) {
     ApplyChannel.send(new Discord.MessageEmbed()
@@ -136,7 +227,15 @@ function SendToChannel(Answers, User) {
         {name:"6.1) If you are applying for Soldier, Rate your PVPskills from 1-10 and tell us what client you use.", value:Answers[7].content},
         {name:"**6.2)** If you are applying for Explorer, Rate you eltraflight from 1-10 and tell us what client you use.", value:Answers[8].content},
         {name:"**7)**If you are not contacted about your application within a maximum of 3 days, It has likely been denied and you will need to wait another month before applying again.", value:Answers[9].content},
-        ).setFooter("bot is in beta, so please manage yourself the user."))
+        ).setFooter("Instructions: ;accept User IsPvp IsBuild IsAdvent where you replace all those is.. with 0 or 1 depending on what roles you want him to have."))
+}
+function getRoleFromMention(mention, message) {
+    if (!mention) return;
+
+	if (mention.startsWith('<@&') && mention.endsWith('>')) {
+        mention = mention.slice(3, -1);
+		return message.guild.roles.cache.get(mention);
+	}
 }
 
-bot.login(process.env.BOT_TOKEN)
+bot.login('ODA0MjM2NTM4OTg0NTI5OTIw.YBJZpA.ffQxr-l5T8rvPhRPbILTdOWP6rY')
